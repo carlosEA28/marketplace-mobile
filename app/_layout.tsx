50cacc545322affdbd5cfda8e03081fcb98d59ee
carlosEA28 <carloseaduarte28@gmail.com>
2025-08-09 14:28:35 -0300

    feat: add inital splash screen setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,31 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
 import "./global.css";
 
+// Set the animation options. This is optional.
+SplashScreen.setOptions({
+  duration: 1000,
+  fade: true,
+});
+
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
+  const [loaded] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hide();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
   return <Stack />;
 }
